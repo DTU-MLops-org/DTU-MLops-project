bc870a7bbff105243d8d18a8ea389cf98ac73d6f
JulieTTNguyen <julienguyen454@gmail.com>
2026-01-09 14:37:04 +0100

    train.py updated

--- src/mlops/train.py
+++ src/mlops/train.py
@@ -1,20 +1,20 @@
 from mlops.model import Model
-from mlops.data import MyDataset
+from mlops.data import fetch_cards
 import typer
 import torch
 
 def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10):
-    train_set, test_set =  # FIXME:missing how to correctly load data 
+    train_set, test_set = fetch_cards() # FIXME:missing how to correctly load data 
     model = Model()
     train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size)
     test_dataloader = torch.utils.data.DataLoader(test_set, batch_size=batch_size)
 
     loss_fn = torch.nn.BCELoss() # Using binary cross entropy 
-    s_weight = 1 
     r_weight = 1
+    s_weight = 1 
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
-    statistics = {"train_loss": [], "train_accuracy": []}
+    statistics = {"train_loss": [], "train_accuracy_suit": [], "train_accuracy_rank": []}
     for epoch in range(epochs):
         model.train()
         for i, (img, target) in enumerate(train_dataloader):
@@ -27,7 +27,9 @@ def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10):
 
             r_accuracy = (y_pred['rank'].argmax(dim=1) == target['rank']).float().mean().item()
             s_accuracy = (y_pred['suit'].argmax(dim=1) == target['suit']).float().mean().item()
-            statistics["train_accuracy"].append((s_accuracy,r_accuracy))
+
+            statistics["train_accuracy_rank"].append(r_accuracy)
+            statistics["train_accuracy_suit"].append(s_accuracy)
 
             if i % 100 == 0:
                 print(f"Epoch {epoch}, iter {i}, loss: {loss.item()}")
