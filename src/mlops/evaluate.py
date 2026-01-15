446f8b6ed631bcd0aefc172b9343edf4245ba395
avkollerup <s234136@dtu.dk>
2026-01-15 10:50:06 +0100

    added changes from ruff

--- src/mlops/evaluate.py
+++ src/mlops/evaluate.py
@@ -4,20 +4,17 @@ from mlops.data import load_data
 import torch
 import typer
 import wandb
-from pathlib import Path
-#import matplotlib.pyplot as plt
+# from pathlib import Path
+# import matplotlib.pyplot as plt
 
-DEVICE = torch.device(
-    "cuda" if torch.cuda.is_available()
-    else "mps" if torch.backends.mps.is_available()
-    else "cpu"
-)
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+
 
 def evaluate(model_checkpoint: str, batch_size: int = 32) -> None:
     """Evaluate a trained model."""
     print("Evaluating like my life depended on it")
     print(model_checkpoint)
-    
+
     # WandB init
     run = wandb.init(
         project="playing-cards-mlops",
@@ -39,27 +36,34 @@ def evaluate(model_checkpoint: str, batch_size: int = 32) -> None:
     suit_correct = 0
     both_correct = 0
     n = 0
-    
+
     with torch.no_grad():
+<<<<<<< HEAD
         for img, targets in test_dataloader:
             img = (img.float() / 255.0).to(DEVICE) # convert to float in [0,1]
             targets = targets.to(DEVICE, dtype=torch.long)
             rank_targets = targets[:, 0]
             suit_targets = targets[:, 1]
+=======
+        for img, rank_targets, suit_targets in test_dataloader:
+            img = img.to(DEVICE)
+            rank_targets = rank_targets.to(DEVICE)
+            suit_targets = suit_targets.to(DEVICE)
+>>>>>>> e823aad (added changes from ruff)
 
             out = model(img)
             rank_pred = out["rank"].argmax(dim=1)
             suit_pred = out["suit"].argmax(dim=1)
-            
+
             rank_correct += (rank_pred == rank_targets).sum().item()
             suit_correct += (suit_pred == suit_targets).sum().item()
             both_correct += ((rank_pred == rank_targets) & (suit_pred == suit_targets)).sum().item()
             n += img.size(0)
-            
-    rank_acc  = rank_correct / n
-    suit_acc  = suit_correct / n
+
+    rank_acc = rank_correct / n
+    suit_acc = suit_correct / n
     joint_acc = both_correct / n
-    
+
     # Log in wandB
     wandb.log(
         {
@@ -69,6 +73,11 @@ def evaluate(model_checkpoint: str, batch_size: int = 32) -> None:
             "eval/num_samples": n,
         }
     )
+<<<<<<< HEAD
     print(f"rank_accuracy {rank_acc}, eval/suit_accuracy {suit_acc}, joint_accuracy {joint_acc}, num_samples {n}")
+=======
+
+
+>>>>>>> e823aad (added changes from ruff)
 if __name__ == "__main__":
     typer.run(evaluate)
--- tasks.py
+++ tasks.py
@@ -6,11 +6,13 @@ WINDOWS = os.name == "nt"
 PROJECT_NAME = "mlops"
 PYTHON_VERSION = "3.13"
 
+
 # Project commands
 @task
 def preprocess_data(ctx: Context) -> None:
     """Preprocess data."""
     ctx.run(f"uv run src/{PROJECT_NAME}/data.py", echo=True, pty=not WINDOWS)
+
 
 @task
 def train(ctx: Context) -> None:
@@ -22,11 +24,13 @@ def evaluate(ctx: Context) -> None:
     """Evaluate model."""
     ctx.run(f"uv run src/{PROJECT_NAME}/evaluate.py models/model.pth", echo=True, pty=not WINDOWS)
 
+
 @task
 def test(ctx: Context) -> None:
     """Run tests."""
     ctx.run('uv run coverage run --source=src --omit="tests/*,/tmp/*" -m pytest tests/', echo=True, pty=not WINDOWS)
     ctx.run('uv run coverage report -m -i --omit="tests/*,/tmp/*"', echo=True, pty=not WINDOWS)
+
 
 @task
 def docker_build(ctx: Context, progress: str = "plain") -> None:
@@ -34,19 +38,19 @@ def docker_build(ctx: Context, progress: str = "plain") -> None:
     ctx.run(
         f"docker build -t train:latest . -f dockerfiles/train.dockerfile --progress={progress}",
         echo=True,
-        pty=not WINDOWS
+        pty=not WINDOWS,
     )
     ctx.run(
-        f"docker build -t api:latest . -f dockerfiles/api.dockerfile --progress={progress}",
-        echo=True,
-        pty=not WINDOWS
+        f"docker build -t api:latest . -f dockerfiles/api.dockerfile --progress={progress}", echo=True, pty=not WINDOWS
     )
+
 
 # Documentation commands
 @task
 def build_docs(ctx: Context) -> None:
     """Build documentation."""
     ctx.run("uv run mkdocs build --config-file docs/mkdocs.yaml --site-dir build", echo=True, pty=not WINDOWS)
+
 
 @task
 def serve_docs(ctx: Context) -> None:
